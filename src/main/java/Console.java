c51e00f80bc7887dc238dd3a0c5be819559ed01e
kendrang <kendrang@users.noreply.github.com>
2019-10-26 15:51:21 -0400

    did stuff in main

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Console {
+    public static void print(String output, Object... args) {
+        System.out.printf(output, args);
+    }
+
+    public static void println(String output, Object... args) {
+        print(output + "\n", args);
+    }
+
+    public static String getStringInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public static Integer getIntegerInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Integer userInput = scanner.nextInt();
+        return userInput;
+    }
+
+    public static Double getDoubleInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Double userInput = scanner.nextDouble();
+        return userInput;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,35 @@
 public class Main {
 
     public static void main(String[] args){
-        
+
+        boolean turnOn = true;
+
+        Console.println("Welcome to the ATM ! Would you like to make a new account or do you have an existing account?");
+        double inputValue = Console.getIntegerInput("Choose: 1 or 2 \n 1. I have an existing account \n 2. I want to make a new Account\n" +
+                "3. Exit ATM "
+        );
+
+        int choice;
+        choice = Console.getIntegerInput();
+
+        switch(choice){
+            case 1:
+                Console.println( "Welcome back! \n Please input your username: ");
+
+                break;
+            case 2:
+                Console.println("Thank you for choosing our bank! Please input your first name:");
+
+                break;
+            case 3:
+                turnOn= false;
+                break;
+
+            default:
+                Console.println("Error: invalid option");
+
+                break;
+        }
+
     }
 }
