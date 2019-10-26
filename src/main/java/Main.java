38ebcc1d55c81800c6c3094b2994fc5adc401268
kendrang <kendrang@users.noreply.github.com>
2019-10-26 17:34:03 -0400

    worked on main functions more

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,76 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
 
         boolean turnOn = true;
 
-        Console.println("Welcome to the ATM ! Would you like to make a new account or do you have an existing account?");
-        double inputValue = Console.getIntegerInput("Choose: 1 or 2 \n 1. I have an existing account \n 2. I want to make a new Account\n" +
-                "3. Exit ATM "
+        Console.println("Welcome to the ATM ! \n                                        \n" +
+                "***     ***              ***              \n" +
+                "***     ***              ***              \n" +
+                "***     ***              ***              \n" +
+                "***     ***     *****    ***   *** ***    \n" +
+                "***     ***    *******   ***   ********   \n" +
+                "***********   ****  ***  ***   **** ****  \n" +
+                "***********   ***   ***  ***   ***   ***  \n" +
+                "***     ***   *********  ***   ***   ***  \n" +
+                "***     ***   *********  ***   ***   ***  \n" +
+                "***     ***   ***        ***   ***   ***  \n" +
+                "***     ***   ****       ***   ****  ***  \n" +
+                "***     ***    ********  ***   ********   \n" +
+                "***     ***     *******  ***   *** ***    \n" +
+                "                               ***        \n" +
+                "                               ***        \n" +
+                "                               ***  \n==========++=========" +
+                " \nWould you like to make a new account or do you have an existing account?");
+        Integer inputValue = Console.getIntegerInput("Choose: an Option \n 1. I have an existing account \n 2. I want to make a new Account\n 3. Exit ATM "
         );
 
-        int choice;
-        choice = Console.getIntegerInput();
-
-        switch(choice){
+        switch(inputValue){
             case 1:
-                Console.println( "Welcome back! \n Please input your username: ");
+                String usernameValue = Console.getStringInput( "Welcome back! \n Please input your username: ");
+
+
+//                if (pinNumValue== usernamePinNum ){
+//                    call the transaction methods here
+//                }
+                for(int i=1;i<=1000;i++)
+                {
+                    try
+                    {
+                        Integer pinNumValue = Console.getIntegerInput( " Please input your four digit pin number: ");
+                        if(pinNumValue<=9999 && pinNumValue>=1000)
+                        {
+                        }
+                        else
+                        {
+                            System.out.println("Pin must be four digit");
+
+                        }
+
+
+                    }
+                    catch(InputMismatchException e)
+                    {
+
+                        System.out.println("Error use numbers not alphabets or characters");
+                    }
+                }
+
+
 
                 break;
             case 2:
                 Console.println("Thank you for choosing our bank! Please input your first name:");
+                String firstName = Console.getStringInput("First name:");
+                String lastName = Console.getStringInput("Last name:");
+                System.out.println("Welcome " + firstName +  " \n Your new username is :" + firstName+lastName  );
 
                 break;
             case 3:
@@ -35,4 +84,5 @@ public class Main {
         }
 
     }
+
 }
